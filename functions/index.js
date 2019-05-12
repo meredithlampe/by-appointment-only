9e1309dae59ec31242b12436481683174f8ede4d
Meredith Lampe <merlam@fb.com>
2019-05-12 13:52:58 -0400

    add date string to metadata saved with submission

--- functions/index.js
+++ functions/index.js
@@ -3,13 +3,12 @@ const admin = require('firebase-admin');
 admin.initializeApp();
 
 exports.submitForm = functions.https.onRequest((req, res) => {
-  // Grab the text parameter.
-  console.log(req);
-  console.log(req.body);
-  console.log(req.query);
+
+	// get timestamp for submissoin
+	let date = new Date();
 
   let submission = {
-  	timestamp: 'today',
+  	date: date.toString(),
   	fields: req.body
   };
   return admin.database().ref('/submissions/' + req.body.formHostID + "/" + req.body.formID).push(submission).then((snapshot) => {
